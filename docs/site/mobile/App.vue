3590fda77068c85e6e749ff9f56ac8f0cad982a1
huangwanneng2 <china.heath.hwn@gmail.com>
2019-12-25 04:13:28 +0800

    文档不展示标题bug修复

--- docs/site/mobile/App.vue
+++ docs/site/mobile/App.vue
@@ -8,85 +8,81 @@
       @click-left="onBack"
     />
     <keep-alive>
-      <router-view :weapp="weapp" />
+      <router-view :weapp="weapp"/>
     </keep-alive>
   </div>
 </template>
 
 <script>
-function getQueryString(name) {
-  const arr = location.search.substr(1).split('&');
-  for (let i = 0, l = arr.length; i < l; i++) {
-    const item = arr[i].split('=');
-    if (item.shift() === name) {
-      return decodeURIComponent(item.join('='));
+  function getQueryString(name) {
+    const arr = location.search.substr(1).split('&');
+    for (let i = 0, l = arr.length; i < l; i++) {
+      const item = arr[i].split('=');
+      if (item.shift() === name) {
+        return decodeURIComponent(item.join('='));
+      }
     }
+    return '';
   }
-  return '';
-}
 
-export default {
-  computed: {
-    title() {
-      return this.$route.meta.title || '';
+  export default {
+    computed: {
+      title() {
+        return this.$route.meta.title || '';
+      },
+
+      showNav() {
+        return getQueryString('hide_nav') !== '1';
+      },
+
+      weapp() {
+        return getQueryString('weapp') === '1';
+      }
     },
 
-    showNav() {
-      return getQueryString('hide_nav') !== '1';
-    },
-
-    weapp() {
-      return getQueryString('weapp') === '1';
+    methods: {
+      onBack() {
+        history.back();
+      }
     }
-  },
-
-  beforeCreate() {
-    this.tips = 'Tips: 当前预览的是 Vue 版 Vant 的示例，少部分功能可能与小程序版有出入，请以文档描述和实际效果为准。';
-  },
-
-  methods: {
-    onBack() {
-      history.back();
-    }
-  }
-};
+  };
 </script>
 
 <style lang="less">
-@import '../../../src/style/var';
+  @import '../../../src/style/var';
 
-body {
-  min-width: 100vw;
-  color: @text-color;
-  font-family: 'PingFang SC', Helvetica, 'STHeiti STXihei', 'Microsoft YaHei', Tohoma, Arial, sans-serif;
-  line-height: 1;
-  background-color: #f7f8fa;
-  -webkit-font-smoothing: antialiased;
-}
-
-::-webkit-scrollbar {
-  width: 0;
-  background: transparent;
-}
-
-.van-doc-nav-bar {
-  height: 56px;
-  line-height: 56px;
-
-  .van-nav-bar__title {
-    font-size: 17px;
-    text-transform: capitalize;
+  body {
+    min-width: 100vw;
+    color: @text-color;
+    font-family: 'PingFang SC', Helvetica, 'STHeiti STXihei', 'Microsoft YaHei', Tohoma, Arial, sans-serif;
+    line-height: 1;
+    background-color: #f7f8fa;
+    -webkit-font-smoothing: antialiased;
   }
 
-  .van-icon {
-    color: @gray-6;
-    font-size: 24px;
-    cursor: pointer;
+  ::-webkit-scrollbar {
+    width: 0;
+    background: transparent;
   }
-}
 
-.van-doc-demo-section {
-  margin-top: -56px;
-  padding-top: 56px;
-}
+  .van-doc-nav-bar {
+    height: 56px;
+    line-height: 56px;
+
+    .van-nav-bar__title {
+      font-size: 17px;
+      text-transform: capitalize;
+    }
+
+    .van-icon {
+      color: @gray-6;
+      font-size: 24px;
+      cursor: pointer;
+    }
+  }
+
+  .van-doc-demo-section {
+    margin-top: -56px;
+    padding-top: 56px;
+  }
 </style>
