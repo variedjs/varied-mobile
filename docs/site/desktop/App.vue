afec1e212838673b4061480980d66181d7ea99ba
huangwanneng2 <china.heath.hwn@gmail.com>
2020-03-03 11:36:47 +0800

    底部添加备案号

--- docs/site/desktop/App.vue
+++ docs/site/desktop/App.vue
@@ -9,66 +9,82 @@
       :current-simulator="currentSimulator"
       @switch-version="onSwitchVersion"
     >
-      <router-view @changeDemoURL="onChangeDemoURL" />
+      <router-view @changeDemoURL="onChangeDemoURL"/>
     </van-doc>
+    <div
+      @click="jump"
+      class="bottom"
+      style="height: 50px;
+      line-height:50px;
+      text-align: center;
+      background-color: #fff;
+      color: #108EE9;
+      font-size: 12px;
+      cursor: pointer">
+      备案号：晋ICP备19014733号-1
+    </div>
   </div>
 </template>
 
 <script>
-import pkgJson from '../../../package.json';
-import docConfig, { github, versions } from '../doc.config';
+  import pkgJson from '../../../package.json';
+  import docConfig, {github, versions} from '../doc.config';
 
-export default {
-  data() {
-    this.github = github;
-    this.versions = versions;
+  export default {
+    data() {
+      this.github = github;
+      this.versions = versions;
 
-    return {
-      simulators: [`mobile.html${location.hash}`],
-      demoURL: ''
-    };
-  },
-
-  computed: {
-    base() {
-      return ``;
+      return {
+        simulators: [`mobile.html${location.hash}`],
+        demoURL: ''
+      };
     },
 
-    config() {
-      return docConfig;
+    computed: {
+      base() {
+        return ``;
+      },
+
+      config() {
+        return docConfig;
+      },
+
+      currentSimulator() {
+        const {name} = this.$route;
+        return name && name.indexOf('demo') !== -1 ? 1 : 0;
+      }
     },
 
-    currentSimulator() {
-      const { name } = this.$route;
-      return name && name.indexOf('demo') !== -1 ? 1 : 0;
-    }
-  },
+    methods: {
+      onChangeDemoURL(url) {
+        this.simulators = [this.simulators[0], url];
+      },
 
-  methods: {
-    onChangeDemoURL(url) {
-      this.simulators = [this.simulators[0], url];
-    },
-
-    onSwitchVersion(version) {
-      if (version !== pkgJson.version) {
+      onSwitchVersion(version) {
+        if (version !== pkgJson.version) {
+        }
+      },
+      jump() {
+        window.location.href = 'http://www.beian.miit.gov.cn';
       }
     }
-  }
-};
+  };
 </script>
 
 <style lang="less">
-.van-doc-intro {
-  padding-top: 20px;
-  font-family: 'Dosis', 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
-  text-align: center;
+  .van-doc-intro {
+    padding-top: 20px;
+    font-family: 'Dosis', 'Source Sans Pro', 'Helvetica Neue', Arial, sans-serif;
+    text-align: center;
 
-  p {
-    margin-bottom: 20px;
+    p {
+      margin-bottom: 20px;
+    }
   }
-}
-  .van-doc{
-    code{
+
+  .van-doc {
+    code {
       background-color: #f2f2f2;
       padding: 10px;
       border-radius: 3px;
